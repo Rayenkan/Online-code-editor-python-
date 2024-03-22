9c61b625a32d0f8b3ab54e0e3243f70d983b21cd
Your Name <ac485561@gmail.com>
2024-03-22 15:48:16 +0100

    final version

--- frontend/src/components/pyCode.jsx
+++ frontend/src/components/pyCode.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const PyCode = ({mode ,  sendDataToParent }) => {
+const PyCode = ({ mode, sendDataToParent }) => {
   const [code, setCode] = useState("");
   const handleChange = (e) => {
     setCode(e.target.value);
@@ -8,25 +8,31 @@ const PyCode = ({mode ,  sendDataToParent }) => {
   };
 
   const handleTab = (event) => {
+    const { selectionStart, selectionEnd } = event.target;
     if (event.key === "Tab") {
       event.preventDefault();
-      const { behindCurson, aheadCursor } = event.target;
       const newCode =
-        code.substring(0, behindCurson) +
-        "  " +
-        code.substring(aheadCursor);
+        code.substring(0, selectionStart) + "  " + code.substring(selectionEnd);
+      setCode(newCode);
+      sendDataToParent(newCode);
+    } else if (event.key === "Enter" && code[selectionStart - 1] === ":") {
+      event.preventDefault();
+      const newCode =
+        code.substring(0, selectionStart) +
+        "\n       " +
+        code.substring(selectionEnd);
       setCode(newCode);
       sendDataToParent(newCode);
     }
   };
 
   return (
-    <div class={mode}> 
+    <div class={mode}>
       <textarea
         className="mx-1 w-[98%] h-[30vh] md:min-h-[75vh] resize-none  p-2 border mt-2 border-gray-300 dark:bg-gray-900 dark:text-white  focus:outline-none"
-
         onChange={handleChange}
         onKeyDown={handleTab}
+        value={code}
       />
     </div>
   );
